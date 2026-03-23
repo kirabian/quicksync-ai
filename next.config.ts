32a7d47d609e31550f05b77dd8482cb9862afbdb
kirabian <fabiansyahalghiffarireal@gmail.com>
2026-03-23 19:06:49 +0700

    config: Refine Next.js configuration by moving reactCompiler, adjusting PWA settings, and forcing webpack for builds.

--- next.config.ts
+++ next.config.ts
@@ -5,14 +5,11 @@ const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
-  skipWaiting: true,
 });
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    reactCompiler: true,
-  }
+  reactCompiler: true,
 };
 
 export default withPWA(nextConfig);
